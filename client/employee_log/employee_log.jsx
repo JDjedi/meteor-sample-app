17aaea13b51a596abc9ae556505126626f0b2b88
JDjedi <jcdiaz1201@gmail.com>
2018-01-15 11:09:42 -0600

    displaying collection data fully functional now

--- client/employee_log/employee_log.jsx
+++ client/employee_log/employee_log.jsx
@@ -5,13 +5,23 @@ import { render } from 'react-dom';
 
 import './employee_log.html';
 
-Meteor.subscribe('employees');
+//Meteor.subscribe('employees');
 
 
 if (Meteor.isClient) {
-	Template.employeeLogTemplate.helpers({
+	Template.list.helpers({
 		employeeList: function(){
 			return Employees.find({}).fetch();
 		}
 	});
-}
+
+	Template.list.onCreated(function() {
+		var self = this;
+		self.autorun(function() {
+			self.subscribe('employees');
+		})
+	});
+};
+
+
+//up next notes:proceed to next vid and figure out how to only display emp log to signed in users
